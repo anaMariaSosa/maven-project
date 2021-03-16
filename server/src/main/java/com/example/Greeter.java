0427a3bef6c767f1c9c4b87e218099ed6e6de752
ana <anamaria.sosaberlanga@gmail.com>
2021-03-16 18:14:59 +0100

    change hello agin

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,6 +14,6 @@ public class Greeter {
 
   //TODO: Add javadoc comment
   public String greet(String someone) {
-    return String.format("Hello, anaaaaa %s!", someone);
+    return String.format("Hello, anaaaaa cuchura %s!", someone);
   }
 }
